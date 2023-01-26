343a0e3292774b3d4d3ef2ba0d56d232b4acd73e
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-01-26 11:28:12 -0500

    Accessed required data

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=" \
       "b1453baed8fde4578038dc"
 
 request = requests.get(url)
+# data = request.text
+# The above method returns a string. We need something more structured to access the data
 data = request.json()
 
-print(type(data))
+for article in data['articles']:
+    print("Title: " + article['title'])
+    print("Description: " + article['description'])
+
+print(data)
